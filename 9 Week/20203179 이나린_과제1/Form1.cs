24aaf11c0ec1b404ae253ae0492126ed847d9c2d
narinn-star <skfls2618@naver.com>
2020-05-28 23:03:02 +0900

    Assignment_9

--- 9 Week/20203179 이나린_과제1/Form1.cs
+++ 9 Week/20203179 이나린_과제1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,79 @@ namespace _20203179_이나린_과제1
 {
     public partial class Form1 : Form
     {
+        double result = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+            result = 0;
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num = double.Parse(textBox1.Text);
+                result += num;
+                label1.Text = result.ToString();
+            }
+            catch(Exception ee)
+            {
+                label1.Text = ee.Message;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num = double.Parse(textBox1.Text);
+                result -= num;
+                label1.Text = result.ToString();
+            }
+            catch (Exception ee)
+            {
+                label1.Text = ee.Message;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num = double.Parse(textBox1.Text);
+                result = result * num;
+                label1.Text = result.ToString();
+            }
+            catch (Exception ee)
+            {
+                label1.Text = ee.Message;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "0")
+                {
+                    double num = double.Parse(textBox1.Text);
+                    result = result / num;
+                    label1.Text = result.ToString();
+                }
+                               
+            }
+            catch(Exception ee)
+            {               
+                label1.Text = ee.Message;
+            }
         }
     }
 }
---
+++ 9 Week/20203179 이나린_과제4/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20203179_이나린_과제4
+{
+    public partial class Form1 : Form
+    {
+        string step = "";
+        double result = 0;
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            label1.Text = "수행단계 : ";
+            label2.Text = "계산결과 : 결과가 나오는 곳입니다!";
+            label3.Text = "계산 수행 횟수";
+            textBox1.Text = "";
+            textBox1.Focus();
+            step = "";
+            result = 0;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num01 = double.Parse(textBox1.Text);
+                
+                label1.Text = "수행단계: 0 + " + num01;
+                
+                result += num01;
+                label2.Text = "계산결과 : " + result;
+                
+                count += 1;
+                label3.Text = "계산 수행 횟수 " + count.ToString();
+
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+            catch (Exception ee)
+            {
+                label2.Text = "계산결과 : " + ee.Message;
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num02 = double.Parse(textBox1.Text);
+
+                label1.Text = "수행단계: 0 - " + num02;
+
+                result -= num02;
+                label2.Text = "계산결과 : " + result;
+
+                count += 1;
+                label3.Text = "계산 수행 횟수 " + count.ToString();
+
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+            catch (Exception ee)
+            {
+                label2.Text = "계산결과 : " + ee.Message;
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num03 = double.Parse(textBox1.Text);
+
+                label1.Text = "수행단계: 0 * " + num03;
+
+                result = result * num03;
+                label2.Text = "계산결과 : " + result;
+
+                count += 1;
+                label3.Text = "계산 수행 횟수 " + count.ToString();
+
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+            catch (Exception ee)
+            {
+                label2.Text = "계산결과 : " + ee.Message;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num04 = double.Parse(textBox1.Text);
+
+                label1.Text = "수행단계: 0 / " + num04;
+
+                count += 1;
+                label3.Text = "계산 수행 횟수 " + count.ToString();
+
+                textBox1.Text = "";
+                textBox1.Focus();
+
+                if (textBox1.Text != "0")
+                {
+                    result = result / num04;
+                    label2.Text = "계산결과 : " + result;
+                }
+                
+                
+            }
+            catch (Exception ee)
+            {
+                label2.Text = "계산결과 : " + ee.Message;
+            }
+        }
+    }
+}
