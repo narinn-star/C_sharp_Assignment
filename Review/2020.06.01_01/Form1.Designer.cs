fcab79e507f4c113e6a3a2aef64628244f2e44ca
narinn-star <skfls2618@naver.com>
2020-06-01 23:55:24 +0900

    REVIEW

---
+++ Review/2020.06.01_01/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace _2020._06._01_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(32, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 137);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "\r\n\r\n      for 반복문을 사용\r\n      17 부터 100 까지의 수를 17 로 나누어           \r\n      떨어지는 수를 " +
+    "출력하는 프로그램\r\n\r\n      나머지를 구하는 Operator  \"%\" 사용\r\n\r\n\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "확 인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(246, 182);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "종 료";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "결과가 나오는 곳입니다";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Review/2020.06.01_01/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020._06._01_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "";
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i % 17 == 0)
+                {
+                    result += i.ToString() + ", ";
+                }
+
+                label2.Text = "17로 나누어 떨어지는 수 : " + result.TrimEnd(',', ' ');
+            }
+        }
+    }
+}
