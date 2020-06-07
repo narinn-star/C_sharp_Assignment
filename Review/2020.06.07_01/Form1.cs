a7a527eed07650b5485fd4c50617d5bf83f7f74b
narinn-star <skfls2618@naver.com>
2020-06-07 18:43:38 +0900

    REVIEW

---
+++ Review/2020.06.07_01/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020._06._07_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            label4.Text = "결과가 나오는 곳입니다";
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                uint Max = uint.Parse(textBox1.Text);
+                uint Plus = uint.Parse(textBox2.Text);
+                double sum = 0;
+                double sum02 = 1;
+
+                if (Max < Plus) label4.Text = "증가값은 최대값보다 작은 수를 입력하세요!";
+                else
+                {
+                    for (uint i = 1; i <= Max; i += Plus)
+                    {
+                        sum += i;
+                        sum02 = sum02 * i;
+                    }
+                    label4.Text = "1 부터 입력된 최대 값: " + Max + " 까지   증가값: " + Plus + " 로 반복한 결과"
+                        + "\n\n<< i 의 더하기 합계 값: " + sum + " >>"
+                        + "\n\n<< i 의 곱하기 합계 값: " + sum02 + " >>";
+                }
+            }
+            catch (Exception ee)
+            {
+                label4.Text = ee.Message + "\n다시 입력하세요!";
+            }
+        }
+    }
+}
---
+++ Review/2020.06.07_02/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace _2020._06._07_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Focus();
+            label3.Text = "결과가 나오는 곳입니다!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 9;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 0;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                uint num = uint.Parse(textBox1.Text);
+                string output = "";
+
+                if (num > 100) label3.Text = "100 이하의 수를 입력하세요!";
+                else
+                {
+                    for (uint i = 1; i <= num; i++)
+                    {
+                        if (i % 10 == 1) output += "\n";
+                        
+                            if (i % 3 == 0)
+                            {
+                                output += " 짝 ";
+                            }
+                            else
+                            {
+
+                                output += " " + i + " ";
+                            }
+                        
+                    }
+                    label3.Text = output;
+                }
+            }
+            catch (Exception ee)
+            {
+                label3.Text = ee.Message;
+            }
+        }
+    }
+}
